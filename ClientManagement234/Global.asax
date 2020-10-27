908714703b870bd135b71392a605400055d629a6
James Shoemaker <james.shoemaker99@gmail.com>
2020-10-27 13:52:05 -0500

    Add project files.

---
+++ ClientManagement234/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClientManagement234.MvcApplication" Language="C#" %>
---
+++ ClientManagement234/Models/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientManagement234.Models
+{
+    public class Client
+    {
+    
+        
+            public enum Status
+            // Client/Status
+            {
+                WORKING,
+                IN_PROGRESS,
+                COMPLETED
+            }
+            public enum Rating
+            // Client/Rating
+            {
+                GREAT,
+                GOOD,
+                POOR,
+                BAD,
+                NONE
+            }
+
+    }
+}
